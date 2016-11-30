3dcbce0b26626785a66828f17ea3257defb7efda
Duncan Doyle <ddoyle@redhat.com>
2016-11-30 12:01:54 +0100

    Initial upgrade to version 6.4.

--- init.sh
+++ init.sh
@@ -1,73 +1,74 @@
-#!/bin/sh 
+#!/bin/sh
 DEMO="Loan Realtime Decision Server Demo"
 AUTHORS="Edson Tirelli, Eric D. Schabell"
 PROJECT1="git@github.com:"
 PROJECT2="jbossdemocentral/brms-realtime-decision-server-demo.git"
 PRODUCT="JBoss BRMS"
-JBOSS_HOME=./target/jboss-eap-6.4
+TARGET=./target
+JBOSS_HOME=$TARGET/jboss-eap-7.0
 SERVER_DIR=$JBOSS_HOME/standalone/deployments
 SERVER_CONF=$JBOSS_HOME/standalone/configuration
 SERVER_BIN=$JBOSS_HOME/bin
 SUPPORT_DIR=./support
 SRC_DIR=./installs
 PRJ_DIR=./projects
-BRMS=jboss-brms-installer-6.2.0.BZ-1299002.jar
-EAP=jboss-eap-6.4.0-installer.jar
-EAP_PATCH=jboss-eap-6.4.4-patch.zip
-VERSION=6.2
+BRMS=jboss-brms-6.4.0.GA-deployable-eap7.x.zip
+EAP=jboss-eap-7.0.0-installer.jar
+#EAP_PATCH=jboss-eap-6.4.4-patch.zip
+VERSION=6.4
 
 # wipe screen.
-clear 
+clear
 
 echo
 echo "##############################################################################"
-echo "##                                                                          ##"   
+echo "##                                                                          ##"
 echo "##  Setting up the ${DEMO}                       ##"
-echo "##                                                                          ##"   
-echo "##                                                                          ##"   
+echo "##                                                                          ##"
+echo "##                                                                          ##"
 echo "##    ##### ####   ###   ####  ####    ####   ####    #   #    ####         ##"
 echo "##      #   #   # #   # #     #        #   #  #   #  # # # #  #             ##"
 echo "##      #   ####  #   #  ###   ###     ####   ####   #  #  #   ###          ##"
 echo "##    # #   #   # #   #     #     #    #   #  #  #   #     #      #         ##"
 echo "##    ###   ####   ###  ####  ####     ####   #   #  #     #  ####          ##"
-echo "##                                                                          ##"   
-echo "##                                                                          ##"   
+echo "##                                                                          ##"
+echo "##                                                                          ##"
 echo "##  brought to you by ${AUTHORS}                       ##"
-echo "##                                                                          ##"   
+echo "##                                                                          ##"
 echo "##  ${PROJECT1}                                                         ##"
 echo "##   ${PROJECT2}                ##"
-echo "##                                                                          ##"   
+echo "##                                                                          ##"
 echo "##############################################################################"
 echo
 
 command -v mvn -q >/dev/null 2>&1 || { echo >&2 "Maven is required but not installed yet... aborting."; exit 1; }
 
-# make some checks first before proceeding.	
+# make some checks first before proceeding.
 if [ -r $SRC_DIR/$EAP ] || [ -L $SRC_DIR/$EAP ]; then
 	echo JBoss EAP sources are present...
 	echo
 else
-	echo Need to download $EAP package from the Customer Portal 
+	echo Need to download $EAP package from the Customer Portal
 	echo and place it in the $SRC_DIR directory to proceed...
 	echo
 	exit
 fi
 
-if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
-	echo Product patches are present...
-	echo
-else
-	echo Need to download $EAP_PATCH package from the Customer Portal 
-	echo and place it in the $SRC_DIR directory to proceed...
-	echo
-	exit
-fi
+#if [ -r $SRC_DIR/$EAP_PATCH ] || [ -L $SRC_DIR/$EAP_PATCH ]; then
+#	echo Product patches are present...
+#	echo
+#else
+#	echo Need to download $EAP_PATCH package from the Customer Portal
+#	echo and place it in the $SRC_DIR directory to proceed...
+#	echo
+#	exit
+#fi
 
 if [ -r $SRC_DIR/$BRMS ] || [ -L $SRC_DIR/$BRMS ]; then
 	echo JBoss BRMS sources are present...
 	echo
 else
-	echo Need to download $BRMS package from the Customer Portal 
+	echo Need to download $BRMS package from the Customer Portal
 	echo and place it in the $SRC_DIR directory to proceed...
 	echo
 	exit
@@ -91,36 +92,38 @@ if [ $? -ne 0 ]; then
 	exit
 fi
 
+#echo
+#echo "Applying JBoss EAP 6.4.4 patch now..."
+#echo
+#$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
+#
+#if [ $? -ne 0 ]; then
+#	echo
+#	echo Error occurred during JBoss EAP patching!
+#	exit
+#fi
+
 echo
-echo "Applying JBoss EAP 6.4.4 patch now..."
+echo "Deploying JBoss BRMS now..."
 echo
-$JBOSS_HOME/bin/jboss-cli.sh --command="patch apply $SRC_DIR/$EAP_PATCH"
+unzip -qo $SRC_DIR/$BRMS -d $TARGET
 
 if [ $? -ne 0 ]; then
 	echo
-	echo Error occurred during JBoss EAP patching!
-	exit
-fi
-
-echo
-echo "JBoss BRMS installer running now..."
-echo
-java -jar $SRC_DIR/$BRMS $SUPPORT_DIR/installation-brms -variablefile $SUPPORT_DIR/installation-brms.variables
-
-if [ $? -ne 0 ]; then
 	echo Error occurred during $PRODUCT installation!
 	exit
 fi
-
 
 echo
 echo "  - creating kie-server user..."
 echo
 $JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u kieserver -p kieserver1! -ro kie-server --silent
 
-echo "  - enabling demo accounts role setup in application-roles.properties file..."
+echo "  - enabling demo accounts setup..."
 echo
-cp $SUPPORT_DIR/application-roles.properties $SERVER_CONF
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u brmsAdmin -p jbossbrms1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
+$JBOSS_HOME/bin/add-user.sh -a -r ApplicationRealm -u erics -p jbossbrms1! -ro analyst,admin,manager,user,kie-server,kiemgmt,rest-all --silent
+
 
 echo "  - setting up demo projects..."
 echo
@@ -138,9 +141,19 @@ echo "  - making sure standalone.sh for server is executable..."
 echo
 chmod u+x $JBOSS_HOME/bin/standalone.sh
 
-echo "You can now start the ${PRODUCT} with ${SERVER_BIN}/standalone.sh"
 echo
-
-echo "${PRODUCT} ${VERSION} ${DEMO} Setup Complete."
-echo
-
+echo "============================================================================"
+echo "=                                                                          ="
+echo "=  You can now start the $PRODUCT with:                                  ="
+echo "=                                                                          ="
+echo "=   $SERVER_BIN/standalone.sh                               ="
+echo "=                                                                          ="
+echo "=  Login into business central at:                                         ="
+echo "=                                                                          ="
+echo "=    http://localhost:8080/business-central  (u:brmsAdmin / p:jbossbrms1!) ="
+echo "=                                                                          ="
+echo "=  See README.md for general details to run the various demo cases.        ="
+echo "=                                                                          ="
+echo "=  $PRODUCT $VERSION $DEMO Setup Complete.       ="
+echo "=                                                                          ="
+echo "============================================================================"
